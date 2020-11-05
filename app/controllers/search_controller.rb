6b583b99dfab9fe04dc45d5de5c8ec700a695e7b
Judith Pillado <judith.pillado@gmail.com>
2020-11-04 20:10:19 -0600

    Add to index method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,15 @@
 class SearchController < ApplicationController
-  def index; end
+  def index
+    conn = Faraday.new('https://developer.nrel.gov')
+    # Faraday.new should only be the protocol and the domain of what we're calling
+
+    result = conn.get('/api/alt-fuel-stations/v1/nearest.json') do |req|
+      # URI path
+      req.params['location'] = params[:location]
+      req.params['fuel_type'] = 'ELEC'
+      req.params['limit'] = 1
+      req.params['api_key'] = ENV['NREL_API']
+    end
+    @station = JSON.parse(result.body, symbolize_names: true)[:fuel_stations][0]
+  end
 end
