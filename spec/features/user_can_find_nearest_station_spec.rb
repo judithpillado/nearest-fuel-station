ee841564c6acaed8e4d9012a586218ce0104a26d
Judith Pillado <judith.pillado@gmail.com>
2020-11-04 22:56:09 -0600

    Add mapquest information to spec

--- spec/features/user_can_find_nearest_station_spec.rb
+++ spec/features/user_can_find_nearest_station_spec.rb
@@ -12,25 +12,8 @@ RSpec.describe 'Find Nearest Station' do
     expect(page).to have_content('Address: 1225 17th St., Denver, CO, 80202')
     expect(page).to have_content('Fuel Type: ELEC')
     expect(page).to have_content('Access Times: MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified')
-    expect(page).to have_content('Distance of Nearest Station: 0.1 miles')
-    expect(page).to have_content('Travel Time: 1 minute')
-    expect(page).to have_content('Direction Instructions: Turn left onto Lawrence St Destination will be on the left')
+    expect(page).to have_content('Distance of Nearest Station: 0.141 miles')
+    expect(page).to have_content('Travel Time: 00:01:04')
+    expect(page).to have_content('Start out going southeast on 17th St toward Larimer St/CO-33.')
   end
 end
-
-# As a user
-# When I visit "/"
-# And I select "Turing" form the start location drop down (Note: Use the existing search form)
-# And I click "Find Nearest Station"
-# Then I should be on page "/search"
-# Then I should see the closest electric fuel station to me.
-# For that station I should see
-# - Name
-# - Address
-# - Fuel Type
-# - Access Times
-# I should also see:
-# - the distance of the nearest station (should be 0.1 miles)
-# - the travel time from Turing to that fuel station (should be 1 min)
-# - The direction instructions to get to that fuel station
-#   "Turn left onto Lawrence St Destination will be on the left"
